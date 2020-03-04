43c8528060a9941b20ba6038e61112db353ae9af
Whitney Kidd <whitneymkidd@gmail.com>
2020-03-04 09:21:04 -0700

    Pass test_it_has_attributes

---
+++ test/artist_test.rb
@@ -0,0 +1,30 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/photograph'
+require './lib/artist'
+
+
+class ArtistTest < Minitest::Test
+  def setup
+    attributes = {
+                id: "2",
+                name: "Ansel Adams",
+                born: "1902",
+                died: "1984",
+                country: "United States"}
+
+    @artist = Artist.new(attributes)
+  end
+
+  def test_it_exists
+    assert_equal Artist, @artist
+  end
+
+  def test_it_has_attributes
+      assert_equal "2", @artist.id
+      assert_equal "Ansel Adams", @artist.name
+      assert_equal "1902", @artist.born
+      assert_equal "1984", @artist.died
+      assert_equal "United States", @artist.country
+  end
+end
