f3527d5ad76426d2134689d56c7b9e052cbe1535
Whitney Kidd <whitneymkidd@gmail.com>
2020-03-04 09:23:00 -0700

    Pass test_can_return_age_at_death

--- lib/artist.rb
+++ lib/artist.rb
@@ -7,4 +7,8 @@ class Artist
     @died = attributes[:died]
     @country = attributes[:country]
   end
+
+  def age_at_death
+    died.to_i - born.to_i
+  end
 end
--- test/artist_test.rb
+++ test/artist_test.rb
@@ -27,4 +27,8 @@ class ArtistTest < Minitest::Test
       assert_equal "1984", @artist.died
       assert_equal "United States", @artist.country
   end
+
+  def test_can_return_age_at_death
+    assert_equal 82, @artist.age_at_death
+  end
 end
